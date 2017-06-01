6b96f52f4aa61388375d93852362417a7247a3a2
Thomas Heller <th.heller@gmail.com>
2017-06-02 00:02:20 +0200

    update example to user the proper relative require

--- cljs-react-app/src/demo/app.cljs
+++ cljs-react-app/src/demo/app.cljs
@@ -2,17 +2,8 @@
   (:require [shadow.markup.react :as html :refer ($ defstyled)]
             [shadow.dom :as dom]
             ["react" :as react]
-            ["react-dom" :refer (render)]))
-
-;; the compiled code lives in node_modules/shadow-cljs/demo.app.js
-;; since node doesn't have the classpath concept of the JVM
-;; we need a relative require path to access JS code
-(def Foo (.. (js/require "../../src/demo/foo") -Foo))
-
-;; the ns form doesn't allow relative requires yet
-;; ideally I want the require to be relative to the source file
-;; this should work at some point, just haven't implemented it yet
-;; ["./Foo" :refer (Foo)]
+            ["react-dom" :refer (render)]
+            ["./foo" :refer (Foo)]))
 
 (defstyled title :h1
   [env]
