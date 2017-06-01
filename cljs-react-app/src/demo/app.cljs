77e550d6495bc4e6eba79bf85df787f816637c8b
Thomas Heller <th.heller@gmail.com>
2017-06-01 17:06:21 +0200

    demo how you could use a React Component written in JS from CLJS

--- cljs-react-app/src/demo/app.cljs
+++ cljs-react-app/src/demo/app.cljs
@@ -1,14 +1,31 @@
 (ns demo.app
-  (:require [shadow.markup.react :as html :refer (defstyled)]
+  (:require [shadow.markup.react :as html :refer ($ defstyled)]
             [shadow.dom :as dom]
+            ["react" :as react]
             ["react-dom" :refer (render)]))
+
+;; the compiled code lives in node_modules/shadow-cljs/demo.app.js
+;; since node doesn't have the classpath concept of the JVM
+;; we need a relative require path to access JS code
+(def Foo (.. (js/require "../../src/demo/foo") -Foo))
+
+;; the ns form doesn't allow relative requires yet
+;; ideally I want the require to be relative to the source file
+;; this should work at some point, just haven't implemented it yet
+;; ["./Foo" :refer (Foo)]
 
 (defstyled title :h1
   [env]
   {:color "red"})
 
 (defn root []
-  (title "hello world!"))
+  (html/div
+    (title "CLJS")
+    ;; $ is a little interop helper
+    ($ Foo {:hello "this is from CLJS!!!"})
+    ;; this works as well
+    (react/createElement Foo #js {:hello "also from CLJS!"})
+    ))
 
 (defn ^:export init []
   ;; can do one time init here
