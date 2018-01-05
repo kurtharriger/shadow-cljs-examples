d9e798da40c776e2292994d308dc80baa22b85bf
Kurt Harriger <kharriger@atlassian.com>
2018-01-05 17:35:15 +1100

    material-ui

--- src/forensics/views.cljs
+++ src/forensics/views.cljs
@@ -1,7 +1,9 @@
 (ns forensics.views
   (:require [re-frame.core :as re-frame]
             [forensics.subs :as subs]
-            ))
+            [reagent-material-ui.core :as ui]))
+
+(defonce theme-defaults {:muiTheme (ui/getMuiTheme ui/darkBaseTheme)})
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
