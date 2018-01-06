4fd6ec7db3014b42280c2d24f09d0b9ea816d571
Kurt Harriger <kharriger@atlassian.com>
2018-01-06 21:41:27 +1100

    fix filename

--- src/cljsjs/material-ui.cljs
+++
@@ -1,6 +0,0 @@
-(ns cljsjs.material-ui
-  (:require ["material-ui" :as MaterialUI]
-            ["material-ui/styles" :as MaterialUIStyles]))
-
-(js/goog.exportSymbol "MaterialUI" MaterialUI)
-(js/goog.exportSymbol "MaterialUIStyles" MaterialUIStyles)
